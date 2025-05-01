25a74627b25eb261135bc372f180b6bb17d77100
jfemon8 <jfemon8@gmail.com>
2025-05-01 17:17:40 +0600

    Added services menu

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Logic for Navbar
+
 document.getElementById("dropdown_btn").addEventListener("click", function () {
     const child = document.getElementById("child_content");
     child.classList.toggle('show-content');
@@ -9,4 +10,29 @@ document.getElementById("nested_dropdown_btn").addEventListener("click", functio
     const child = document.getElementById("nested_child_content");
     child.classList.toggle('show-content');
 })
+
+
+
+// Active clicked package
+
+const serviceCards = document.querySelectorAll('.package-card');
+
+const updateDefaultCard = (c) => {
+    const icon = c.querySelector('span');
+    icon.innerHTML = `<i class="fa-regular fa-circle"></i>`
+}
+
+const handleCardClick = (card) => {
+    serviceCards.forEach(c => {
+        updateDefaultCard(c);
+    })
+    card.querySelector('span').innerHTML = `<i class="fa-solid fa-circle-check package-fa"></i>`;
+
+}
+
+serviceCards.forEach(card => {
+    card.addEventListener('click', () => handleCardClick(card));
+})
+
+
 
